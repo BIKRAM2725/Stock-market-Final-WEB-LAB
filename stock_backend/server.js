7b7549fc9ef0e5ca01f2a6a8130733b649c3a5ed
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-05-14 01:44:08 +0530

    Refactor server to use environment variable for port

--- stock_backend/server.js
+++ stock_backend/server.js
@@ -10,11 +10,14 @@ import stockRoutes from "./routes/stockRoutes.js";
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.use("/api", chatRoutes);
 app.use("/api", stockRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
 });
